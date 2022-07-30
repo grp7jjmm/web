19f48fd950f93c9c7dc3bc330db9743bcf00f120
grp7jjmm <109274949+grp7jjmm@users.noreply.github.com>
2022-07-30 01:59:28 -0400

    Update Web.java

--- selenium/src/main/java/group7jjmm/selenium/Web.java
+++ selenium/src/main/java/group7jjmm/selenium/Web.java
@@ -6,12 +6,14 @@ public class Web {
 	public static void main(String[] args) throws InterruptedException{
 		System.setProperty("webdriver.chrome.driver", "//chromedriver//chromedriver");
 		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox"); // Bypass OS security model
+		options.addArguments("--headless");
 		options.addArguments("start-maximized"); // open Browser in maximized mode
 		options.addArguments("disable-infobars"); // disabling infobars
 		options.addArguments("--disable-extensions"); // disabling extensions
 		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-		options.addArguments("--no-sandbox"); // Bypass OS security model
-		options.addArguments("--headless");
+		
+		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
