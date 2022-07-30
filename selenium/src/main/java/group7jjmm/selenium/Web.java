444c865d55469e4f6ec1e386245201b84075729a
grp7jjmm <109274949+grp7jjmm@users.noreply.github.com>
2022-07-30 00:36:37 -0400

    Add files via upload

---
+++ selenium/src/main/java/group7jjmm/selenium/Web.java
@@ -0,0 +1,13 @@
+package group7jjmm.selenium;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+public class Web {
+	public static void main(String[] args) throws InterruptedException{
+		System.setProperty("webdriver.chrome.driver", "//home//kali//Downloads//chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("http://localhost:8081/mvnwebapp");
+		//driver.findElement(By.xpath("//input[@id='login-username']")).sendKeys("xyzz@yahoo.com");
+	}
+}
