6b53052faf72a28ff290fa38d20635ec8718085e
grp7jjmm <109274949+grp7jjmm@users.noreply.github.com>
2022-07-30 01:54:58 -0400

    Update Web.java

--- selenium/src/main/java/group7jjmm/selenium/Web.java
+++ selenium/src/main/java/group7jjmm/selenium/Web.java
@@ -1,5 +1,6 @@
 package group7jjmm.selenium;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebDriver;
 public class Web {
 	public static void main(String[] args) throws InterruptedException{
