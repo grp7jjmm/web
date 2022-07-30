4be77b54759dff295c69b26c204b756103af8686
grp7jjmm <109274949+grp7jjmm@users.noreply.github.com>
2022-07-30 01:22:11 -0400

    Update Web.java

--- selenium/src/main/java/group7jjmm/selenium/Web.java
+++ selenium/src/main/java/group7jjmm/selenium/Web.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 public class Web {
 	public static void main(String[] args) throws InterruptedException{
-		System.setProperty("webdriver.chrome.driver", "//home//kali//Downloads//chromedriver");
+		System.setProperty("webdriver.chrome.driver", "//chromedriver//chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
