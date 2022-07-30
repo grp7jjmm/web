3ee1201af91952b7d36bb77c9386cb277b5bfed3
grp7jjmm <109274949+grp7jjmm@users.noreply.github.com>
2022-07-30 01:54:02 -0400

    Update Web.java

--- selenium/src/main/java/group7jjmm/selenium/Web.java
+++ selenium/src/main/java/group7jjmm/selenium/Web.java
@@ -4,10 +4,16 @@ import org.openqa.selenium.WebDriver;
 public class Web {
 	public static void main(String[] args) throws InterruptedException{
 		System.setProperty("webdriver.chrome.driver", "//chromedriver//chromedriver");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized"); // open Browser in maximized mode
+		options.addArguments("disable-infobars"); // disabling infobars
+		options.addArguments("--disable-extensions"); // disabling extensions
+		options.addArguments("--disable-gpu"); // applicable to windows os only
+		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+		options.addArguments("--no-sandbox"); // Bypass OS security model
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.get("http://localhost:8081/mvnwebapp");
-		//driver.findElement(By.xpath("//input[@id='login-username']")).sendKeys("xyzz@yahoo.com");
 	}
 }
